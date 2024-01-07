5ec84ec6c1fa27eae72b253c8a030177eaaba44c
Aaron Graab <aarongraab@hotmail.com>
2024-01-06 22:25:31 -0330

    Close to finishing Part 1 solution

--- Advent of Code - 2023 Projects/December 2, 2023/Dec 2 - Part 1 Solution.py
+++ Advent of Code - 2023 Projects/December 2, 2023/Dec 2 - Part 1 Solution.py
@@ -1,34 +1,35 @@
 REDLIMIT = 12
 GREENLIMIT = 13
 BLUELIMIT = 14
+gameBool = True
 gameNumberTotal = 0
 
-myString = "Game 1: 4 red, 5 blue, 9 green; 7 green, 7 blue, 3 red; 16 red, 7 blue, 3 green; 11 green, 11 blue, 6 red; 12 red, 14 blue"
-
-myStringSplit = myString.split(maxsplit=2)
-gameNumber = myStringSplit[1].rstrip(":")
-cubeList = myStringSplit[2].split(";")
-for cubes in cubeList:
+def doCalc(cubes):
     coloursSplit = cubes.split(",")
     for i in coloursSplit:
         i.rstrip(",")
         i.strip(" ")
         myColour = i.split()
-        print(myColour)
-        print("0:", myColour[0], "\n1:", myColour[1])
+        if myColour[1] == "red":
+            if int(myColour[0]) > REDLIMIT:
+                return False
+        if myColour[1] == "green":
+            if int(myColour[0]) > GREENLIMIT:
+                return False
+        if myColour[1] == "blue":
+            if int(myColour[0]) > BLUELIMIT:
+                return False
+        else:
+            return True
 
-    '''
-            if myColour[1] == "red":
-                if int(myColour[0]) <= REDLIMIT:
-                    gameNumberTotal += int(gameNumber)
-            if myColour[1] == "green":
-                if int(myColour[0]) <= GREENLIMIT:
-                    gameNumberTotal += int(gameNumber)
-            if myColour[1] == "blue":
-                if int(myColour[0]) <= BLUELIMIT:
-                    gameNumberTotal += int(gameNumber)
-            else:
-                print("ERROR")
-'''
+with open('input.txt', 'r') as file:
+    for line in file:
+        myStringSplit = line.split(maxsplit=2)
+        gameNumber = myStringSplit[1].rstrip(":")
+        cubeList = myStringSplit[2].split(";")
+        for cubes in cubeList:
+            gameBool = doCalc(cubes)
+        if gameBool:
+            gameNumberTotal += int(gameNumber)
 
 print("Game Number Total is:", gameNumberTotal)
